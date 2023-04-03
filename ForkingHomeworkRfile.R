150588af08550e71916b8882aa27088d05f091d2
lily-stock <116843178+lily-stock@users.noreply.github.com>
2023-04-02 20:37:12 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,10 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+
+as.data.table(diamonds) -> diamonds
+diamonds[cut == "Premium" & price > 7000 & price < 10000] -> diamonds2
+diamonds2[order(-carat), .(carat, cut, price)] -> diamonds3
+diamonds3[c(1:20)] -> diamonds4
+diamonds4
